3eeab30007f264bfcaf008ece13abf89c680d785
makifyegin <mehmetakifyegin@icloud.com>
2026-04-15 22:40:42 +0100

    Add Availability model with validations and tests

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -9,11 +9,13 @@ class Availability < ApplicationRecord
   private
 
   def end_time_after_start_time
-    # think: what do you need to check?
-    # if end_time is before or equal to start_time, add an error
+    return if end_time.blank? || start_time.blank?
+    errors.add(:end_time, "must be after start time") if end_time <= start_time
   end
 
   def date_not_in_past
+    return if date.blank?
+    errors.add(:date, "...") if date < Date.today
     # think: what do you need to check?
     # if date is before today, add an error
   end

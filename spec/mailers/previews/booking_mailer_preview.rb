0c8d7e64c0af1c84467c4da1b3cd3f0fc8ef4873
makifyegin <mehmetakifyegin@icloud.com>
2026-04-22 22:19:34 +0100

    Add email preview for booking verification

--- spec/mailers/previews/booking_mailer_preview.rb
+++ spec/mailers/previews/booking_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/booking_mailer_mailer
 class BookingMailerPreview < ActionMailer::Preview
-
+  def verification_email
+    booking = Booking.last
+    BookingMailer.verification_email(booking)
+  end
 end
